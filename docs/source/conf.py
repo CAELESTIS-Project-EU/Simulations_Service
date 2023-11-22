06ae7392971289ce4e9ab1ce5260731f4e589c3f
Riccardo Cecco <61690228+RiccardoCecco@users.noreply.github.com>
2023-11-22 16:23:53 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,6 +2,15 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+import inspect
+import os
+import subprocess as subp
+import sys
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
